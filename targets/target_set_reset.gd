dbe180c0a5507cce498f811bf6f08bf1d83f6146
Matthew S <cicd@sdggamecollective.com>
2023-08-30 03:36:40 -0500

    Update fuel ramp geo to prevent ball stuck.

---
+++ targets/target_set_reset.gd
@@ -0,0 +1,11 @@
+extends StaticBody3D
+
+@onready var animation_player = $AnimationPlayer
+@export var hit := false
+
+
+func _on_area_3d_body_entered(_body):
+	if !hit:
+		animation_player.play("hit")
+		Print.from(PrintScope.GLOBAL, "target %s hit" % [name], Print.VERBOSE)
+		hit = true
