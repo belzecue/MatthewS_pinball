65e73a8bd54e0abd1d490f30426affe2be0ba41e
Matthew S <cicd@sdggamecollective.com>
2023-08-30 23:54:42 -0500

    Scoops, sorta

---
+++ targets/scoop.gd
@@ -0,0 +1,9 @@
+extends AnimatableBody3D
+
+@onready var animation_player = $AnimationPlayer
+
+
+func _on_area_3d_body_entered(_body):
+	if !animation_player.is_playing():
+		Print.from(PrintScope.GLOBAL, "%s Scoop activated" % [name], Print.VERBOSE)
+		animation_player.play("hit")
