d7f9a1df56296388a9478a4c3f3610a7053ef1da
Matthew S <cicd@sdggamecollective.com>
2023-09-01 01:59:48 -0500

    More sounds!

--- targets/bumper.gd
+++ targets/bumper.gd
@@ -1,9 +1,17 @@
 extends AnimatableBody3D
 
+@export var sound: AudioStreamWAV
 @onready var animation_player = $AnimationPlayer
+@onready var pop_player = $PopPlayer
+
+
+func _ready():
+	pop_player.stream = sound
 
 
 func _on_area_3d_body_entered(_body):
 	if !animation_player.is_playing():
+		if !Global.mute:
+			pop_player.play()
 		Print.from(PrintScope.GLOBAL, "Bumper %s hit" % [name], Print.VERBOSE)
 		animation_player.play("hit")
--- targets/slingshot.gd
+++ targets/slingshot.gd
@@ -1,9 +1,17 @@
 extends AnimatableBody3D
 
+@export var fire: AudioStreamWAV
 @onready var animation_player = $AnimationPlayer
+@onready var fire_sound = $FireSound
+
+
+func _ready():
+	fire_sound.stream = fire
 
 
 func _on_area_3d_body_entered(_body):
 	if !animation_player.is_playing():
 		Print.from(PrintScope.GLOBAL, "Slingshot %s hit" % [name], Print.VERBOSE)
+		if !Global.mute:
+			fire_sound.play()
 		animation_player.play("hit")
