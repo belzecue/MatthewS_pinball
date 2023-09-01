2b9ef7ba3b5378c336a53b40ce13e26f32630bec
Matthew S <cicd@sdggamecollective.com>
2023-09-01 02:05:41 -0500

    Add kickers

---
+++ targets/kicker.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var kick_sound: AudioStreamWAV
+@onready var animation_player = $AnimationPlayer
+@onready var kick_audio = $KickAudio
+
+
+func _ready():
+	kick_audio.stream = kick_sound
+
+
+func _on_area_3d_body_entered(body):
+	if !animation_player.is_playing():
+		await get_tree().create_timer(1).timeout
+		if !Global.mute:
+			kick_audio.play()
+		animation_player.play("kick_ball")
